ff42c29a90656b642f762c17d5bf89fd5471514f
Niktopol <kirill-22ru@yandex.ru>
2022-12-11 21:48:06 +0300

    .

--- main.cpp
+++ main.cpp
@@ -73,7 +73,7 @@ void putOff(std::vector<int> arr, int n, int* sum, int strtln) {
     std::vector <int> a = arr;
     for (int i = 0; i < a.size(); i++) {
         if (a.at(i) == n) {
-            *sum += fact(strtln-(n+1));
+            *sum += fact(strtln - (n + 1));
         }
         else {
             a.erase(a.begin() + i);
@@ -92,9 +92,9 @@ int balls(int c) {
     return sum;
 }
 std::string year21(int bgn, int yr) {
-    std::string clr[] = {"green", "red", "yellow", "white", "black"};
+    std::string clr[] = { "green", "red", "yellow", "white", "black" };
     std::string anml[] = { "rat", "cow", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "chicken", "dog", "pig" };
-    int dlt = (yr-bgn)%60;
+    int dlt = (yr - bgn) % 60;
     if (dlt >= 0) {
         return (anml[dlt % 12] + " " + clr[int((dlt - dlt % 12) / 12)]);
     }
@@ -104,120 +104,139 @@ std::string year21(int bgn, int yr) {
 }
 int len(int n) {
     int c = 0;
-    while (n > 0){
+    while (n > 0) {
         n /= 10;
         c++;
     }
     return c;
 }
-int rever(int n) {
-    int rs = 0;
-    for (int i = 1; i <= len(n); i++) {
-        rs += int(n % int(pow(10,i))/ int(pow(10, i-1))) * pow(10, len(n)-i);
+bool find(char a, std::string d) {
+    for (int i = 0; i < d.size(); i++) {
+        if (d[i] == a) {
+            return true;
+        }
     }
-    return rs;
+    return false;
 }
-bool polinum29(int num) {
-    int bg = 0;
-    int end = 0;
-    if (len(num) % 2 == 0) {
-        bg = int(num / pow(10, int(len(num) / 2)));
-        end = num % int(pow(10, int(len(num) / 2)));
+bool numCheck(std::string s) {
+    std::string d = "0123456789";
+    for (int i = 0; i < s.size(); i++) {
+        if (!find(s[i], d)) {
+            return false;
+        }
     }
-    else {
-        bg = int(num / pow(10, int(len(num) / 2) + 1));
-        end = num % int(pow(10, int(len(num) / 2)));
+    if (s.size() == 1) {
+        if (s[0] == '0') {
+            return false;
+        }
     }
-    return rever(bg) == end;
+    return true;
 }
-int fndminmx(std::vector<int> a, bool fl){
-    int m = a[0];
-    if (fl){
-        for (int i = 0; i < a.size(); i++){
-            m = (a[i] < m) ? a[i] : m;
-        }
-    }else{
-        for (int i = 0; i < a.size(); i++){
-            m = (a[i] > m) ? a[i] : m;
+bool comp(std::string bg, std::string end) {
+    for (int i = 0; i < bg.size(); i++) {
+        if (bg[i] != end[end.size() - 1 - i]) {
+            return false;
         }
     }
-    return m;
+    return true;
 }
-void sort48(int n){
-    std::vector<int> arr;
-    std::vector<int> b;
-    int a, c, mx;
-    std::cout << "Введите "<< n << " элементов" << std::endl;
-    for (int i = 0; i < n; i++){
-        safe_inp(&a, 0);
-        arr.push_back(a);
+bool polinum29(std::string s) {
+    std::string bg = "";
+    std::string end = "";
+    for (int i = 0; i < int(s.size() / 2); i++) {
+        bg += s[i];
+        end = s[s.size() - 1 - i]+end;
     }
-    b = arr;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n-1; j++){
-            if (b[j+1] < b[j]){
-                c = b[j+1];
-                b[j+1] = b[j];
-                b[j] = c;
-            }
-        }
-    }
-    mx = fndminmx(b, false)+1;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if (b[i] == arr[j]){
-                b[i] = j;
-                arr[j] = mx;
-            }
-        }
-    }
-    std::cout << "Возрастающая последовательность:" << std::endl;
-    for (int i = 0; i < n; i++){
-        std::cout << b[i] << " ";
-    }
-    std::cout << std::endl << "Убывающая последовательность:" << std::endl;
-    for (int i = n-1; i >= 0; i--){
-        std::cout << b[i] << " ";
+    return comp(bg, end);
+}
+void printArr(std::vector<int> a) {
+    for (int i = 0; i < a.size(); i++) {
+        std::cout << a[i] << " ";
     }
     std::cout << std::endl;
 }
-void printMx(std::vector<std::vector<int>> a, int ln){
-    int ab = ln+2;
-    for (int i = 0; i < a.size(); i++){
-        for (int j = 0; j < a.size(); j++){
-            std::cout <<  std::setw(ab) << a.at(i).at(j);
+void sort48(int n) {
+    std::vector<int> arr;
+    int a, c, mx;
+    std::cout << "Введите " << n << " элементов" << std::endl;
+    for (int i = 0; i < n; i++) {
+        safe_inp(&a, 0);
+        arr.push_back(a);
+    }
+    std::cout << "Возрастающие последовательности:" << std::endl;
+    std::vector<int> ind;
+    for (int i = 0; i < n; i++) {
+        if (ind.size() == 0) {
+            ind.push_back(i);
+        }
+        else {
+            if (arr[i - 1] < arr[i]) {
+                ind.push_back(i);
+            }
+            else {
+                if (ind.size() > 1) {
+                    printArr(ind);
+                }
+                ind.clear();
+            }
+        }
+    }
+    std::cout << "Убывающие последовательности:" << std::endl;
+    ind.clear();
+    for (int i = 0; i < n; i++) {
+        if (ind.size() == 0) {
+            ind.push_back(i);
+        }
+        else {
+            if (arr[i - 1] > arr[i]) {
+                ind.push_back(i);
+            }
+            else {
+                if (ind.size() > 1) {
+                    printArr(ind);
+                }
+                ind.clear();
+            }
+        }
+    }
+}
+void printMx(std::vector<std::vector<int>> a, int ln) {
+    int ab = ln + 2;
+    for (int i = 0; i < a.size(); i++) {
+        for (int j = 0; j < a.size(); j++) {
+            std::cout << std::setw(ab) << a.at(i).at(j);
         }
         std::cout << std::endl;
     }
 }
-void rotmx(std::vector<std::vector<int>> a, int rot, int mxel){
+void rotmx(std::vector<std::vector<int>> a, int rot, int mxel) {
     std::vector<std::vector<int>> b;
     int sz = a.size();
     b.resize(sz);
-    for (int i = 0; i < sz; i++){
+    for (int i = 0; i < sz; i++) {
         b.at(i).resize(sz);
     }
-    if (rot < 0){
+    if (rot < 0) {
         rot = 4 + rot;
     }
-    for (int i = 0; i < rot; i ++){
-        for (int j = 0; j < sz; j++){
-            for (int g = 0; g < sz; g++){
-                b[g][sz-1-j] = a[j][g];
+    for (int i = 0; i < rot; i++) {
+        for (int j = 0; j < sz; j++) {
+            for (int g = 0; g < sz; g++) {
+                b[g][sz - 1 - j] = a[j][g];
             }
         }
         a = b;
     }
     printMx(a, mxel);
 }
-void rotate62(int m){
+void rotate62(int m) {
     std::vector <std::vector <int>> arr;
     arr.resize(m);
     int a;
-    std::cout << "Введите "<< m*m << " элементов" << std::endl;
-    int mxel;
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < m; j++){
+    std::cout << "Введите " << m * m << " элементов" << std::endl;
+    int mxel = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
             safe_inp(&a, 0);
             int ln = len(a);
             mxel = (mxel < ln) ? ln : mxel;
@@ -226,31 +245,31 @@ void rotate62(int m){
     }
     printMx(arr, mxel);
     std::cout << "Введите количество поворотов на 90 и их направление (-n = повернуть против часовой n раз, n = повернуть по часовой n раз)" << std::endl;
-    if (safe_inp(&a, -1)){
-        rotmx(arr, a%4, mxel);
+    if (safe_inp(&a, -1)) {
+        rotmx(arr, a % 4, mxel);
     }
-    else{
+    else {
         std::cout << "Неверный ввод" << std::endl;
     }
 }
-std::string makestr(int a, int ln){
+std::string makestr(int a, int ln) {
     std::string dct = "0123456789";
     std::string out = "";
-    for (int i = 0; i < ln; i++){
+    for (int i = 0; i < ln; i++) {
         out += dct[a];
     }
     return out;
 }
-void rec79(int a, int b, int ln, int dir){
-    if (b == 0){
+void rec79(int a, int b, int ln, int dir) {
+    if (b == 0) {
         return;
     }
-    int x = ln + (a*a-ln)/2;
+    int x = ln + (a * a - ln) / 2;
     std::cout << std::setw(x) << makestr(b, ln) << std::endl;
-    if (b == a){
+    if (b == a) {
         dir = -dir;
     }
-    rec79(a,b+dir,ln-(a*dir), dir);
+    rec79(a, b + dir, ln - (a * dir), dir);
 }
 int main() {
     setlocale(0, "");
@@ -258,6 +277,7 @@ int main() {
     int* ab[] = { &a, &b };
     double c = 0, d = 0;
     double* cd[] = { &c, &d };
+    std::string s = "";
     while (true) {
         std::cout << "Введите номер задания" << std::endl;
         safe_inp(&a, -1);
@@ -282,12 +302,13 @@ int main() {
                 break;
             case 3:
                 std::cout << "Введите число" << std::endl;
-                if (safe_inp(&a, -1) and a >= 0) {
-                    if (polinum29(a)) {
-                        std::cout << "Да" << std::endl;
+                std::cin >> s;
+                if (numCheck(s)) {
+                    if (polinum29(s)) {
+                        std::cout << "Полиндром" << std::endl;
                     }
                     else {
-                        std::cout << "Нет" << std::endl;
+                        std::cout << "Не полиндром" << std::endl;
                     }
                 }
                 else {
@@ -305,7 +326,7 @@ int main() {
                 break;
             case 5:
                 std::cout << "Введите порядок матрицы" << std::endl;
-                if (safe_inp(&a, -1) and a > 0) {
+                if (safe_inp(&a, -1) and a > 0 and a < 1000) {
                     rotate62(a);
                 }
                 else {
@@ -314,8 +335,8 @@ int main() {
                 break;
             case 6:
                 std::cout << "Введите чётное число из отрезка [2,8]" << std::endl;
-                if (safe_inp(&a, -1) and a > 0 and a%2 == 0 and a < 10) {
-                    rec79(a,1, a*a, 1);
+                if (safe_inp(&a, -1) and a > 0 and a % 2 == 0 and a < 10) {
+                    rec79(a, 1, a * a, 1);
                 }
                 else {
                     std::cout << "Неверный ввод" << std::endl;
